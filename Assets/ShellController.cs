fc27bcad02c627c26f8d18783e6fea9eee0a2e95
sungeun2001 <luminousken@naver.com>
2022-05-30 15:46:01 +0900

    11주차 탱크 2개 만들고 폭탄 나옴

---
+++ Assets/ShellController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour
+{
+    public ParticleSystem shellExplosion;
+
+    private void OnCollisionEnter (Collision other)
+    {
+        ParticleSystem fire = Instantiate (shellExplosion , transform.position , Quaternion.identity);
+        fire.Play ();
+       
+        Destroy (gameObject);
+        Destroy (fire.gameObject , 2.0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
